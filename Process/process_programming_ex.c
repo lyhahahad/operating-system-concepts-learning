0cd5d55797b322143a43e4d09fc56bb024345acc
glawkdxn0328 <glawkdxn0328@naver.com>
2022-04-22 10:22:21 +0900

    process learning

--- Process/process_programming_ex.c
+++ Process/process_programming_ex.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-// ref : https://quiet-lilac-e5d.notion.site/chapter3-process-concept-abe56556dfd3421f8aba7b507ee64e70
 
 #define MAX_LEN 100 /* The maximum length command */
 
@@ -19,13 +18,11 @@ int main(void) {
     printf("my_shell> ");
     fflush(stdout);
 
-    // A. shell 명령어 입력 by fgets.
+    // A. 사용자에게 문자열(shell 명령어) 입력을 받는다.
     char *input = (char*)malloc(MAX_LEN*sizeof(char));
     fgets(input, MAX_LEN, stdin); 
 
-    // B. 입력 받은 문자열 띄어씌기와 줄바꿈 기준으로 분할 - strtok
-    // input이 없을 경우엔 goto를 써서 While의 끝부분으로 보낸다.
-    // 입력을 받고 바로 input 유무를 판별하지 않는 이유는 fgets는 입력이 없더라도 NULL 값을 반환하지 않기 때문이다.
+    // B. A에서 입력받은 문자열을 " \n" 단위로 쪼갠다.
 	int i = 0;
 	args[0] = strtok(input," \n");
 	while(args[i] != NULL){
@@ -36,8 +33,7 @@ int main(void) {
 		goto no_input;
 	}
 
-    // C. 쪼갠 문자열에서 exit, & 문자열이 있는지 찾는다. by strcmp
-    // &을 입력받으면 background에서 해당 프로세스를 실행한다.
+    // C. B에서 쪼갠 문자열을 비교한다.	
    if(strcmp(args[0],"exit")==0){
 	break;
 	}
@@ -45,41 +41,38 @@ int main(void) {
 	args[i-1] = NULL;
 	background = 1;	
 	}
-// ref : https://quiet-lilac-e5d.notion.site/chapter3-process-concept-abe56556dfd3421f8aba7b507ee64e70
+
+
 
     /**
      * D. fork()를 통해 자식 프로세스를 생성한다.
-     * 
+     * (1) 자식 프로세스가 execvp()를 호출하도록 할 것
+     * (2) 만약 입력받은 문자에 &가 포함되어 있으면,
+     *     부모 프로세스는 wait() 호출하지 않음 (background process)
      */
 
+    // Fork Error에 대한 예외처리
     pid_t pid = fork();
-    // 에러가 발생한 경우
     if (pid < 0) {
       perror("Fork error");
       exit(0);
     }
-    // 자식 프로세스
     else if(pid == 0){
-	      sleep(1);
-        // 입력 받은 명령어를 실행한다.
-        if(execvp(args[0], args)<0){
-          printf("command error, not found \n");
-        }
-      }
-    //부모 프로세스
-    else{
-	    if(!background){
-	        printf("waiting for child, not background proccess \n");
-          //자식 프로세스가 끝날 때까지 기다린다.
+    	sleep(1);
+	    if(execvp(args[0], args)<0){
+	 	  printf("command error, not found \n");
+	    } 
+    }else{
+      if(!background){
+          printf("waiting for child, not background proccess \n");
           waitpid(pid, &status,0);
           printf("child process complete \n");	
-	      } 
-    else{
-          printf("background process \n");
-          background = 0;
-	      }	
-	    }
+      }else{
+        printf("background process \n");
+        background = 0;
+      }	
+	}
      no_input:;
-    }
+  }
   return 0;
 }
